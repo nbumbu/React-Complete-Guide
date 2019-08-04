9b74ebf95293de1ff9d35b6fad7e45e02e109959
Nick <sahas023@gmail.com>
2019-08-04 14:46:13 +0200

    Using Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import classes from './Person.css'
+import PropTypes from 'prop-types';
+import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
 
     render() {
         console.log('[Person.js] is rendering...........');
@@ -11,11 +21,22 @@ class Person extends Component {
             <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} year old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                // ref={(inputEl) => {this.inputElement = inputEl}} 
+                ref={this.inputElementRef}
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} />
             </Aux>
         )
     }
+}
 
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -27,7 +27,8 @@ class App extends Component {
       ],
       otherState: 'Other value',
       showPersons: false,
-      showCockpit: true
+      showCockpit: true,
+      changeCounter: 0
     }
   }
 
@@ -58,8 +59,11 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
     });
   }
 
