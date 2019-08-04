dbb3653f767d48b611553990c9ffcdc83a826bb4
Nick <sahas023@gmail.com>
2019-08-04 13:56:56 +0200

    React Fragment usage example

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.css'
 import Aux from '../../../hoc/Aux';
 
@@ -7,11 +7,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] is rendering...........');
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} year old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Fragment>
         )
     }
 
